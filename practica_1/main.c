d65b7d021eeac3fbbac11271e39a133d7ad86b2e
vazquezjose <60374151+vazquezjose@users.noreply.github.com>
2020-02-10 20:33:50 -0600

    Corrige error de identación

--- practica_1/main.c
+++ practica_1/main.c
@@ -2,6 +2,6 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
- printf("Hola mundo. JOSE ISAIAS VAZQUEZ MACIAS 1894608\n");
+	printf("Hola mundo. JOSE ISAIAS VAZQUEZ MACIAS 1894608\n");
 	return 0;
 }
