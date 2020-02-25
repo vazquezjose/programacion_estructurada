24ecafa3af38e740295515d03e3ebc3396ae50c8
vazquezjose <60374151+vazquezjose@users.noreply.github.com>
2020-02-24 20:38:02 -0600

    practica_4

---
+++ practica_4/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char c;
+float num1, num2, res = 0;
+
+int main()
+{
+	printf("\t\tCALCULADORA\n");
+	printf("Introduzca la primer cifra: ");
+	scanf("%f", &num1);
+	printf("Introduzca el tipo de operacion [+,-,*,/]: ");
+	scanf(" %c", &c);
+	printf("Introduzca la segunda cifra: ");
+	scanf("%f", &num2);
+	switch (c)
+	{
+		case '+':
+			res = num1 + num2;
+			break;
+		case '-':
+			res = num1 - num2;
+			break;
+		case '*':
+			res = num1 * num2;
+			break;
+		case '/':
+			res = num1 / num2;
+			break;
+		default:
+			printf("El tipo de operacion es invalido.\n");
+	}
+	printf("El resultado es: %.f\n", res);
+	system("pause");
+	return 0;
+}
