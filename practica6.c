a292301d930b8af1a8c52d4b4c61a0b51c45bdc4
vazquezjose <60374151+vazquezjose@users.noreply.github.com>
2020-06-01 17:44:11 -0500

    practica6

---
+++ practica6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int i = 0, suma_total = 0, v;
+	float promedio = 0.0f;
+	
+	do
+	{
+		printf("Introduzca un valor entero: ");
+		scanf("%i", &v);
+		if (v >= 0)
+		{
+			suma_total = suma_total + v;
+			i++;
+		}
+	}
+	while (v >= 0);
+	
+	if (suma_total != 0)
+		promedio = suma_total / i;
+	printf("El promedio es de: %.2f.\n", promedio);
+	
+	system("pause");
+	return 0;
+}
