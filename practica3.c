1af192cd40285c00cc4039036f506e2e98e5de73
vazquezjose <60374151+vazquezjose@users.noreply.github.com>
2020-06-01 17:32:26 -0500

    practica3

---
+++ practica3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char c;
+	int ascii;
+	
+	printf("Ingresa un caracter:\n");
+	c = getchar();
+	ascii = (int) c;
+	
+	if (ascii >= 48 && ascii <= 57)
+		printf("El caracter introducido es un numero.\n");
+	else
+		if (ascii >= 65 && ascii <= 90)
+			printf("El caracter introducido es una letra mayuscula.\n");
+		else
+			if (ascii >= 97 && ascii <= 122)
+				printf("El caracter introducido es una letra minuscula.\n");
+			else
+				printf("El caracter introducido es un simbolo.\n");
+	system("pause");
+	return 0;
+}
